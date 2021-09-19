7086e9efb0bef2d4e1c1218241657a9f6717e01c
zyeongPark <piiy10141997@gmail.com>
2021-09-19 21:00:38 +0900

    start

--- week03_UITableView/AppDelegate.swift
+++ week03_UITableView/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+ //환경일기장 만들기
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
