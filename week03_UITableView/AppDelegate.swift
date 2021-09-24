a641a7615e9e5b8afe3bc14fb608e9b08514e76f
zyeongPark <piiy10141997@gmail.com>
2021-09-24 23:17:37 +0900

    new

--- week03_UITableView/AppDelegate.swift
+++ week03_UITableView/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
- //환경일기장 만들기!! -> 예쁜 tableview 만들기!
+ //환경일기장 만들기!! -> 예쁜 tableview 만들기 피그마 에셋 사용
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
