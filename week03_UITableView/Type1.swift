f4487c4f3b4de67df0e50b3ae527283c32c710a6
zyeongPark <piiy10141997@gmail.com>
2021-09-30 23:46:42 +0900

    newsApp

---
+++ week03_UITableView/Type1.swift
@@ -0,0 +1,12 @@
+//
+//  Type1.swift
+//  week03_UITableView
+//
+//  Created by 박지영 on 2021/09/30.
+//
+
+import UIKit
+
+class Type1 : UITableViewCell {
+    @IBOutlet weak var LabelText : UILabel!
+}
